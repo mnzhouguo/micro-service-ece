84c1d5aa512cac41ef6d80b873ccc9d024402cb6
zhouguo <mnzhouguo@msn.cn>
2016-12-12 14:13:01 +0800

    ee

--- spring-boot-sample-helloworld/src/main/java/com/example/HelloController.java
+++ spring-boot-sample-helloworld/src/main/java/com/example/HelloController.java
@@ -1,14 +1,20 @@
 package com.example;
 
 import org.springframework.web.bind.annotation.PathVariable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory ;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+	protected static Logger logger=LoggerFactory.getLogger("HelloController");
+
 	@RequestMapping("/")
 	public String helloworld(){
+		
+		logger.debug("访问主页");
 		return "Hello world!";
 	}
 	
